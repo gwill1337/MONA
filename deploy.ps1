1e985b38d7518c0899d4230b021768961cca77ba
gwill1337 <gwyllig@gmail.com>
2026-08-27 19:46:15 +0300

    added scripts for deploying and docker-compose files

--- deploy.ps1
+++ deploy.ps1
@@ -6,13 +6,14 @@ param (
 
 if ($All -or $Build) {
     Write-Host "=== Building Docker images ===" -ForegroundColor Cyan
-    docker build -t mona-fastapi:local -f ./docker/dockerfile.fastapi .
-    docker build -t mona-react:local -f ./docker/dockerfile.react .
-    docker build -t mona-celery:local -f ./docker/dockerfile.celery .
+    docker build -t mona-fastapi:local -f ./docker/Dockerfile.fastapi .
+    docker build -t mona-react:local -f ./docker/Dockerfile.react .
+    docker build -t mona-celery:local -f ./docker/Dockerfile.celery .
 }
 
 if ($All -or $Deploy) {
     Write-Host "=== Deploying infrastructure (Terraform) ===" -ForegroundColor Cyan
+    helm repo update
     terraform -chdir=terraform init
     terraform -chdir=terraform apply -auto-approve
 }
